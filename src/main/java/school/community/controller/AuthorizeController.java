2d6701405a0860bd365cc07a5c27ff78fd424f57
CDX <1138224907@qq.com>
2020-04-22 20:09:52 +0800

    4.22 commit

---
+++ src/main/java/school/community/controller/AuthorizeController.java
@@ -0,0 +1,47 @@
+package school.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import school.community.dto.AccessTokenDTO;
+import school.community.dto.GithubUser;
+import school.community.prodiver.GithubProvider;
+
+/**
+ * @Auther:cdx
+ * @Date:2020-04-22
+ * @Description:school.community.controller
+ * @Version:1.0
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser =githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
+        return "index";
+    }
+
+}
+
