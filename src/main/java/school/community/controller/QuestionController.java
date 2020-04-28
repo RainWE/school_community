431e7d371fb891ae88b646a4e3ff491a8ac5b616
CDX <1138224907@qq.com>
2020-04-28 16:23:20 +0800

    add question details

---
+++ src/main/java/school/community/controller/QuestionController.java
@@ -0,0 +1,31 @@
+package school.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import school.community.dto.QuestionDTO;
+import school.community.service.QuestionService;
+
+/**
+ * @Auther:cdx
+ * @Date:2020-04-28
+ * @Description:school.community.controller
+ * @Version:1.0
+ */
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public  String question(@PathVariable(name="id") Integer id, Model model){
+
+        QuestionDTO questionDTO=questionService.getById(id);
+        model.addAttribute("question",questionDTO);
+        return "question";
+    }
+}
+
